16ac9bbb3bfbad6b42d6f5e28ea4a4fe99511a1b
Paolo Cargnin <cargnin.paolo@gmail.com>
2014-10-18 15:18:22 +0200

    Working on widths...

---
+++ grunt/config.requireJs.js
@@ -0,0 +1,17 @@
+requirejs.config({
+   baseUrl: '../js',
+   paths  : {
+   },
+   // shim   : {
+   //    angular            : {
+   //       exports: 'angular'
+   //    },
+   //    'angular-ui-router': {
+   //       exports: 'angular',
+   //       deps   : ['angular']
+   //    },
+   //    lodash             : {
+   //       exports: '_'
+   //    }
+   // },
+});
